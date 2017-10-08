6d74c4f73b5128e34ce462cbdcb005e5f18f64af
Rafael Specht <rafaelspdasilva@gmail.com>
2017-10-08 15:24:13 -0300

    Adding globals to eslint file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
   "extends": "standard",
   globals: {
-    Image: true
+    Image: true,
+    AudioContext: true,
+    Filter: true,
+    plotFiltered: true,
+    splitRGB: true,
+    XMLHttpRequest: true
   }
 }
